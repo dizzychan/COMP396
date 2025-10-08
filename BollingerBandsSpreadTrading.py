0ce8c3b55d8e8dc421d471bec0a56624d754f30e
Dai Chan <sgjche50@liverpool.ac.uk>
2025-10-08 10:30:23 +0100

    update

--- BollingerBandsSpreadTrading.py
+++ BollingerBandsSpreadTrading.py
@@ -25,3 +25,57 @@ class BollingerBandsSpreadTrading:
         self.positions = []
         self.trades = []
 
+    def calculate_spread(self, asset1_prices, asset2_prices, method='ratio'):
+        """
+        Calculate spread between two assets
+
+        Parameters:
+        asset1_prices (pd.Series): Prices of first asset
+        asset2_prices (pd.Series): Prices of second asset
+        method (str): 'ratio' for price ratio, 'difference' for price difference
+
+        Returns:
+        pd.Series: Spread series
+        """
+        if method == 'ratio':
+            # Use log ratio to make spread more stationary
+            spread = np.log(asset1_prices / asset2_prices)
+        elif method == 'difference':
+            # Simple price difference
+            spread = asset1_prices - asset2_prices
+        else:
+            # Normalized difference
+            spread = (asset1_prices / asset1_prices.iloc[0]) - (asset2_prices / asset2_prices.iloc[0])
+
+        return spread
+
+    def calculate_bollinger_bands_for_spread(self, spread):
+        """
+        Calculate Bollinger Bands for the spread
+
+        Parameters:
+        spread (pd.Series): Spread between assets
+
+        Returns:
+        pd.DataFrame: DataFrame with spread and Bollinger Bands
+        """
+        df = pd.DataFrame()
+        df['Spread'] = spread
+
+        # Calculate moving average of spread
+        df['MA'] = df['Spread'].rolling(window=self.window).mean()
+
+        # Calculate standard deviation of spread
+        df['STD'] = df['Spread'].rolling(window=self.window).std()
+
+        # Calculate Bollinger Bands
+        df['Upper_Band'] = df['MA'] + (self.num_std * df['STD'])
+        df['Lower_Band'] = df['MA'] - (self.num_std * df['STD'])
+
+        # Calculate Z-score (standardized spread)
+        df['Z_Score'] = (df['Spread'] - df['MA']) / df['STD']
+
+        return df
+
+    #the next step is to generate trading signals
+
